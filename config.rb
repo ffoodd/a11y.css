b04807913276483b1efc49d61d31cc9af59d70e1
Gaël Poupard <gael.poupard@gmail.com>
2013-11-25 20:37:58 +0100

    Nouveautés
    Quelques corrections sont apportées et un premier lot de recommandations
    a fait son apparition. À relire et tester !

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ images_dir = "img"
 javascripts_dir = ""
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
