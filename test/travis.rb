1088e8c883b140182d4e5ab17098175cfdaf95ba
gael <gael@ornikar.com>
2015-12-31 12:02:29 +0100

    Test for #190 again again

--- test/travis.rb
+++ test/travis.rb
@@ -1,8 +1,8 @@
-# -*- encoding : utf-8 -*-
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 #!/usr/bin/env ruby
 result = `sass sass/a11y-fr.scss css/a11y-fr.css`
 raise result unless $?.to_i == 0
 raise 'When compiled the module should output some CSS' unless File.exist?('css/a11y-fr.css')
 puts 'Sass compiled successfully'
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
