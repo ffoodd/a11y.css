c7c69ac29b9a73912d58f83ba166a11958e65490
ffoodd <gael.poupard@gmail.com>
2016-04-13 14:38:22 +0200

    Added Autoprefixer to Hologram's CSS + for #211

--- tasks/hologram-sass.js
+++ tasks/hologram-sass.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var options = require('./helpers/options');
+
 var base = './docs/Hologram/test-assets';
 
 module.exports = function () {
   return gulp.src(base + '/sass/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer(options.autoprefixer))
     .pipe(gulp.dest(base + '/static/'));
 };
