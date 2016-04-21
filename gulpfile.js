bffa3c072868e9fa9bcb2cc512c6159ba985f616
Hugo Giraudel <hugo.giraudel@gmail.com>
2016-04-21 11:40:22 +0200

    Removed and updated a few Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,15 @@ var gulp = require('gulp');
 
 // Single tasks
 gulp.task('sass', require('./tasks/sass'));
-gulp.task('css', ['sass'], require('./tasks/css'));
 gulp.task('sassdoc', require('./tasks/sassdoc'));
-gulp.task('sassdoc-html', require('./tasks/sassdoc-html'));
 gulp.task('hologram-js', require('./tasks/hologram-js'));
 gulp.task('hologram-sass', require('./tasks/hologram-sass'));
 gulp.task('hologram', ['hologram-sass', 'hologram-js'], require('./tasks/hologram'));
-gulp.task('hologram-html', require('./tasks/hologram-html'));
 gulp.task('scss-lint', require('./tasks/scss-lint'));
 gulp.task('css-lint', require('./tasks/css-lint'));
 gulp.task('watch', require('./tasks/watch'));
 
 // Meta tasks
 gulp.task('docs', ['sassdoc', 'hologram']);
-gulp.task('html', ['sassdoc-html', 'hologram-html']);
 gulp.task('lints', ['scss-lint', 'css-lint']);
-gulp.task('default', ['css']);
+gulp.task('default', ['sass']);
