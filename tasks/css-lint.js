968459706e67bfbd158a1c5fcf79de161a2b1438
Gaël Poupard <gael.poupard@orange.com>
2017-11-20 14:24:48 +0100

    fix: CSS Lint configuration (formatter + disabled rules) to close #270

--- tasks/css-lint.js
+++ tasks/css-lint.js
@@ -6,29 +6,11 @@ var plumber = require('gulp-plumber');
 var options = require('./helpers/options');
 var onError = require('./helpers/onError');
 
+csslint.addFormatter('csslint-stylish');
+
 module.exports = function () {
   gulp.src(options.destination + '/**/*.css')
     .pipe(plumber({ errorHandler: onError }))
-    .pipe(csslint(path.resolve(__dirname, '..', 'configs', '.csslintrc')))
-    .pipe(csslint.reporter(customReporter));
+    .pipe(csslint(path.resolve(__dirname, '..', 'configs', 'csslintrc.json')))
+    .pipe(csslint.formatter('stylish'));
 };
-
-function customReporter (file) {
-  gutil.log(gutil.colors.cyan(file.csslint.errorCount) + ' errors in ' + gutil.colors.magenta(file.path));
-
-  file.csslint.results.forEach(function (result) {
-    var message = gutil.colors.green(' Line: ' + result.error.line) +
-      gutil.colors.cyan(' Column: ' + result.error.col) + ' ' +
-      gutil.colors.magenta(result.error.message) + ' ' +
-      gutil.colors.gray(result.error.rule.desc) + ' ' +
-      gutil.colors.red('Browsers: ' + result.error.rule.browsers);
-
-    if (result.error.type === 'warning') {
-      message = gutil.colors.yellow.bold('[Warning]') + message;
-    } else {
-      message = gutil.colors.red.bold('[' + result.error.type + ']') + message;
-    }
-
-    gutil.log(message);
-  })
-}
