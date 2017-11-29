b5c690a7de7d37500030eb2f1ecda551b6ce1b06
notabene <github@nota-bene.org>
2017-11-29 18:30:04 +0100

    webext: Localisation mechanisms in place

---
+++ webextension/popup/locales.js
@@ -0,0 +1,21 @@
+// locales
+
+// locales available in a11y.css
+const locales = [
+	"en",
+	"fr",
+	"pt-br",
+	"ru",
+	"zh"
+];
+
+const localeStrings = {
+	"en":{
+		"btnOutline":"Outline focus",
+		"btnAllyCss":"Show a11y.css"
+	},
+	"fr":{
+		"btnOutline": "Encadrer le focus",
+		"btnAllyCss": "Afficher a11y.css"
+	}
+}
---
+++ webextension/popup/popup.js
@@ -0,0 +1,33 @@
+const BROWSER = chrome || browser;
+const BROWSER_STRING = chrome ? 'chrome' : 'browser';
+// const STORAGE = BROWSER.storage.sync || BROWSER.storage.local;
+// TODO later : storage
+
+// Set language first
+var localeNormalCase = 'en'; // default locale, may happen to be capitalised, cf pt-BR
+var locale; // this will retrieve a lower-case locale, used for file names etc.
+
+function setLocale() {
+	// first find if there is a locale we can use
+	let isLocaleFoundYet = false;
+	let ualang = navigator.languages; // grab the navigator's languages
+	console.log('ualang', ualang);
+	ualang.forEach(function (key) {
+		if (locales.includes(key.toLowerCase()) && !isLocaleFoundYet) {
+			localeNormalCase = key; // new locale
+			isLocaleFoundYet = true;
+			return;
+		}
+	});
+	locale = localeNormalCase.toLowerCase();
+	document.body.setAttribute('lang',locale);
+	document.getElementById('btnOutline').innerHTML = localeStrings[locale]['btnOutline'];
+	var btns = document.getElementById('home').getElementsByTagName('button');
+	console.log(btns.toString());
+	for(let i=0 ; i < btns.length ; i++) {
+		console.log(i);
+		btns[i].innerHTML = localeStrings[locale][btns[i].getAttribute('id')] ? localeStrings[locale][btns[i].getAttribute('id')] : '?';
+	}
+}
+
+setLocale();
