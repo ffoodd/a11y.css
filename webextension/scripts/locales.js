2822498a77d2d7a961d3458d59fefe50e1aa5e34
notabene <github@nota-bene.org>
2017-12-04 11:26:15 +0100

    webext: Locales for a11ycss

--- webextension/scripts/locales.js
+++ webextension/scripts/locales.js
@@ -12,21 +12,35 @@ const locales = [
 // all locale strings needed for the script
 const localeStrings = {
 	"en":{
+		"_LOCALE":"English",
+		"a11ycssLanguageLabel":"Language",
+		"a11ycssLevel_advice": "Everything",
+		"a11ycssLevel_obsolete": "Obsolete Stuff, Warnings, and Errors",
+		"a11ycssLevel_warning": "Warnings and Errors",
+		"a11ycssLevel_error": "Only Errors",
+		"a11ycssMinimumNoticeLevel": "Minimum Notice Level",
 		"btnA11ycssBack":"&lt; &lt; Back",
 		"btnOutline":"Outline focus",
 		"btnOutlineRemove":"Remove focus outline",
 		"btnAllycss":"Show a11y.css",
 		"h1Allycss":"a11y.css",
 		"h1Allycss2":"a11y.css: settings",
-		"h1Others":"Other accessibility-related tools"
+		"h1Others":"Other accessibility-related tools",
 	},
 	"fr":{
+		"_LOCALE":"Français",
+		"a11ycssLanguageLabel": "Langue",
+		"a11ycssLevel_advice": "Tout",
+		"a11ycssLevel_obsolete": "Éléments obsolètes, Avis et Erreurs",
+		"a11ycssLevel_warning": "Avis et Erreurs",
+		"a11ycssLevel_error": "Erreurs seulement",
+		"a11ycssMinimumNoticeLevel": "Minimum Notice Level",
 		"btnA11ycssBack":"&lt; &lt; Retour",
 		"btnOutline": "Encadrer le focus",
 		"btnOutlineRemove":"Retirer l'encadrement du focus",
 		"btnAllycss": "Afficher a11y.css",
 		"h1Allycss": "a11y.css",
 		"h1Allycss2": "a11y.css : réglages",
-		"h1Others": "Autres outils liés à l'accessibilité"
+		"h1Others": "Autres outils liés à l'accessibilité",
 	}
 }
