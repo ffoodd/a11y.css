90e4693e13ad32d269f4879e0be7bb2a23bff956
Gaël Poupard <gael.poupard@orange.com>
2018-01-03 14:27:20 +0100

    feat: added .editorconfig file to close #288 + clean up some files

--- tasks/translate.js
+++ tasks/translate.js
@@ -8,12 +8,12 @@ var options = require('./helpers/options');
 var onError = require('./helpers/onError');
 
 module.exports = function () {
-    options.locales.forEach( function(locale) {
-      return fs.createReadStream('locales/' + locale + '.json')
-        .pipe(plumber({ errorHandler: onError }))
-        .pipe(rootbeer({ prefix: '$messages: ' }))
-        .pipe(source(locale + '.json'))
-        .pipe(rename('_' + locale + '.scss'))
-        .pipe(gulp.dest(options.localesdest));
-    });
+  options.locales.forEach( function(locale) {
+    return fs.createReadStream('locales/' + locale + '.json')
+      .pipe(plumber({ errorHandler: onError }))
+      .pipe(rootbeer({ prefix: '$messages: ' }))
+      .pipe(source(locale + '.json'))
+      .pipe(rename('_' + locale + '.scss'))
+      .pipe(gulp.dest(options.localesdest));
+  });
 };
