b306766b013558eff07455e5c1441b8220063475
notabene <github@nota-bene.org>
2018-01-16 16:06:12 +0100

    Zip file renamed in Gulp task for naming coherence

--- tasks/webextension.js
+++ tasks/webextension.js
@@ -7,6 +7,6 @@ var onError = require('./helpers/onError');
 module.exports = function () {
   return gulp.src(options.webextension)
     .pipe(plumber({ errorHandler: onError }))
-    .pipe(zip('a11y-css-webextension.zip'))
+    .pipe(zip('a11ycss-webextension.zip'))
     .pipe(gulp.dest('./'));
 };
