b949c7290ad776757d4c91b06ac7a8b9f09094a6
ffoodd <gael.poupard@gmail.com>
2018-11-27 20:57:12 +0100

    chore: upgrade dependencies after #333

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,13 @@ gulp.task('sass', require('./tasks/sass'));
 gulp.task('sassdoc', require('./tasks/sassdoc'));
 gulp.task('hologram-js', require('./tasks/hologram-js'));
 gulp.task('hologram-sass', require('./tasks/hologram-sass'));
-gulp.task('hologram', ['hologram-sass', 'hologram-js'], require('./tasks/hologram'));
+gulp.task('hologram', gulp.series('hologram-sass', 'hologram-js'), require('./tasks/hologram'));
 gulp.task('scss-lint', require('./tasks/scss-lint'));
 gulp.task('css-lint', require('./tasks/css-lint'));
 gulp.task('watch', require('./tasks/watch'));
 gulp.task('translate', require('./tasks/translate'));
 
 // Meta tasks
-gulp.task('docs', ['sassdoc', 'hologram']);
-gulp.task('lints', ['scss-lint', 'css-lint']);
-gulp.task('default', ['sass']);
+gulp.task('docs', gulp.series('sassdoc', 'hologram'));
+gulp.task('lints', gulp.series('scss-lint', 'css-lint'));
+gulp.task('default', gulp.series('sass'));
