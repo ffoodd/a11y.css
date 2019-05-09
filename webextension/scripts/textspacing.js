1322db8875f522808ac0a5ae43d80ca810db1103
ffoodd <gael.poupard@gmail.com>
2019-05-09 22:02:33 +0200

    feat[WIP]: handle tab's URL change, need a way to remove current tab's setting for #292

--- webextension/scripts/textspacing.js
+++ webextension/scripts/textspacing.js
@@ -32,13 +32,28 @@ btnTextspacing.addEventListener('click', function () {
 	storeTextSpacingStatus(!checked);
 });
 
+function textSpacingOnUpdated(tabId, changeInfo, tabInfo) {
+  if (changeInfo.url) {
+    let getStatus = browser.storage.local.get("textSpacingStatus");
+	getStatus.then(
+		// when we got something
+		(item) => {
+			if (item && item.textSpacingStatus && item.textSpacingStatus[tabId]) {
+				// Need to find a way to only remove current tab's value
+			}
+		}, onError
+	);
+  }
+}
+
+browser.tabs.onUpdated.addListener(textSpacingOnUpdated);
+
 function textSpacingOnload() {
 	let getStatus = browser.storage.local.get("textSpacingStatus");
 	getStatus.then(
 		// when we got something
 		(item) => {
 			if (item && item.textSpacingStatus) {
-				console.log(item.textSpacingStatus);
 				browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
 					// If a setting is found for this tab
 					if (item.textSpacingStatus[tabs[0].id]) {
