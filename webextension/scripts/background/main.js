3402ae6766757d7f1b9a1344bfb3ab168f2b4952
Gaël Poupard <gael.poupard@orange.com>
2019-05-10 17:24:48 +0200

    feat[WIP]: switch status to false on tab.onUpdated

--- webextension/scripts/background/main.js
+++ webextension/scripts/background/main.js
@@ -1,16 +1,17 @@
+function onError(error) {
+	console.error(`a11y.css error: ${error}`);
+}
+
 function textSpacingOnUpdated(tabId, changeInfo, tabInfo) {
     if (changeInfo.url) {
         let getStatus = browser.storage.local.get("textSpacingStatus");
         getStatus.then(
-            // when we got something
             (item) => {
-                console.log("item");
-                console.log(item);
                 if (item && item.textSpacingStatus && item.textSpacingStatus[tabId]) {
-                    console.log("textspacing");
-                    console.log(item.textSpacingStatus[tabId]);
-                    // what's are we doing if item.textSpacingStatus[tabId] exist? remove it?
-                    // Need to find a way to only remove current tab's value
+					let textSpacingStatus = item.textSpacingStatus;
+					textSpacingStatus[tabId] = {"status": false};
+					let setting = browser.storage.local.set({ textSpacingStatus });
+					setting.then(null, onError);
                 }
             }
         );
