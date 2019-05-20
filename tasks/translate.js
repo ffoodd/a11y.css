ed16d2cdce0c841f6827bf42913275debb928e17
Gaël Poupard <gael.poupard@orange.com>
2019-05-20 16:56:16 +0200

    fix: translation task

--- tasks/translate.js
+++ tasks/translate.js
@@ -7,13 +7,14 @@ var plumber = require('gulp-plumber');
 var options = require('./helpers/options');
 var onError = require('./helpers/onError');
 
-module.exports = function () {
+module.exports = function (done) {
   options.locales.forEach( function(locale) {
+    console.info('Generate translation Sass map for: ' + locale);
     return fs.createReadStream('locales/' + locale + '.json')
       .pipe(plumber({ errorHandler: onError }))
       .pipe(rootbeer({ prefix: '$messages: ' }))
       .pipe(source(locale + '.json'))
       .pipe(rename('_' + locale + '.scss'))
       .pipe(gulp.dest(options.localesdest));
-  });
+  }, done());
 };
