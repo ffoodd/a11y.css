ab7803a393e1f163f7e46c9d7e703c1467841f01
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2020-01-21 11:42:52 +0100

    feat(assets): Reorganize assets and compile CSS/JS on Eleventy launch

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const a11yCSS = require('./a11y.css')
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(a11yCSS)
 
-  eleventyConfig.addPassthroughCopy('src/assets')
+  eleventyConfig.addPassthroughCopy('src/static')
 
   return {
     dir: {
