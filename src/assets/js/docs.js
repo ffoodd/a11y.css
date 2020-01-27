b86ceca03acdfa1f0d6cae471a6147fdb5b52e60
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2020-01-27 15:48:57 +0100

    feat(bookmarklet): Remove bookmarklet related stuff
    closes #294

--- src/assets/js/docs.js
+++ src/assets/js/docs.js
@@ -1,32 +1,3 @@
-function getFileName () {
-  var base = 'https://ffoodd.github.io/a11y.css/css/';
-  var prefix = 'a11y-';
-  var lang = document.getElementById('language').value || 'en';
-  var level = document.getElementById('minimum-level').value || 'advice';
-  var extension = '.css';
-
-  if (level !== 'advice') {
-    level = '_' + level;
-  } else {
-    level = '';
-  }
-
-  return base + prefix + lang + level + extension;
-}
-
-function getBookmarkletContent () {
-  return [
-    'javascript:',
-    '(function(){',
-    'a11ycss=document.createElement("LINK");',
-    'a11ycss.href="' + getFileName() + '";',
-    'a11ycss.rel="stylesheet";',
-    'a11ycss.media="all";',
-    'document.body.appendChild(a11ycss);',
-    '})();'
-  ].join('');
-}
-
 function activeAnchor (toc) {
   var toc = toc || document.getElementById('toc');
   var currentHref = toc.getElementsByTagName('a');
@@ -51,28 +22,8 @@ function handleTocClick (event) {
   }
 }
 
-function handleFormSubmit(e) {
-  e.preventDefault();
-
-  var link = document.getElementById('bookmarklet--link');
-  link.setAttribute('href', getBookmarkletContent());
-  link.classList.add('done');
-}
-
-function handleFormChange () {
-  var link = document.getElementById('bookmarklet--link');
-  link.classList.remove('done');
-}
-
 // Quand le DOM a fini de charger
 document.addEventListener('DOMContentLoaded', function() {
-  // On écoute la soumission du formulaire de génération
-  var bookmarklink = document.getElementById('bookmarklet');
-  if (bookmarklink) {
-    bookmarklink.addEventListener('submit', handleFormSubmit);
-    bookmarklink.addEventListener('change', handleFormChange);
-  }
-
   // On bricole pour afficher l’ancre active dans le sommaire
   var toc = document.getElementById('toc');
   if (toc) {
