c4775c9439034485f1dc508917dc67543b9fe771
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2020-01-28 10:41:54 +0100

    refactor(TOC): Rewrite TOC script and use `aria-current` to style active link

--- src/assets/js/docs.js
+++ src/assets/js/docs.js
@@ -1,33 +1,26 @@
-function activeAnchor (toc) {
-  var toc = toc || document.getElementById('toc');
-  var currentHref = toc.getElementsByTagName('a');
+function Toc () {
+  const toc = document.getElementById('toc')
 
-  for (i = 0; i < currentHref.length; i++) {
-    if (document.location.href.indexOf(currentHref[i].href) >= 0) {
-      currentHref[i].classList.add('active-test');
+  if (!toc) {
+    return
+  }
+
+  const tocLinks = toc.querySelectorAll('a')
+
+  function setActiveTocLink () {
+    const activeTocLink = toc.querySelector(`a[href="${document.location.hash}"]`)
+
+    if (!activeTocLink) {
+      return
     }
+
+    tocLinks.forEach(link => link.removeAttribute('aria-current'))
+    activeTocLink.setAttribute('aria-current', 'true')
   }
+
+  setActiveTocLink()
+
+  window.addEventListener('hashchange', setActiveTocLink)
 }
 
-function handleTocClick (event) {
-  var link = event.target;
-  var active = document.querySelector('.active-test');
-
-  if (active) {
-    active.removeAttribute('class');
-  }
-
-  if (link.tagName.toLowerCase() === 'a') {
-    link.classList.add('active-test');
-  }
-}
-
-// Quand le DOM a fini de charger
-document.addEventListener('DOMContentLoaded', function() {
-  // On bricole pour afficher l’ancre active dans le sommaire
-  var toc = document.getElementById('toc');
-  if (toc) {
-    toc.addEventListener('click', handleTocClick);
-    activeAnchor(toc);
-  }
-});
+document.addEventListener('DOMContentLoaded', Toc)
