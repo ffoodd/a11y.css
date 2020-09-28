4f3f7d1be6b5056434eaaed2f7824d5191ada4d5
Gaël Poupard <gael.poupard@orange.com>
2020-09-28 17:09:05 +0200

    docs(PrismJS)[WIP]: trying to use 11ty plugin for syntaxhighlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
 const a11yCSS = require('./a11y.css')
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(a11yCSS)
+  eleventyConfig.addPlugin(syntaxHighlight, {
+    templateFormats: ["njk", "md"]
+  })
 
   eleventyConfig.addPassthroughCopy('src/static')
 
