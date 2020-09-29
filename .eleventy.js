a2a2eff72e28c1cb7601337a2d47ed649ab4b84c
Gaël Poupard <gael.poupard@orange.com>
2020-09-29 15:01:53 +0200

    docs(syntax highlighting)[WIP]: drop 11ty plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,7 @@
 const a11yCSS = require('./a11y.css')
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(a11yCSS)
-  eleventyConfig.addPlugin(syntaxHighlight, {
-    templateFormats: ["njk", "md"]
-  })
 
   eleventyConfig.addPassthroughCopy('src/static')
 
