e94eae7e34a3cb6f1ca913b4c9f97822495c490e
Gaël Poupard <gael.poupard@orange.com>
2020-10-27 14:34:16 +0100

    docs(SassDoc)[WIP]: drop plugin draft, aiming to use a11yCSS plugin too

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,7 @@
 const a11yCSS = require('./a11y.css')
-const SassDoc = require('./sassdoc')
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(a11yCSS)
-  eleventyConfig.addPlugin(SassDoc)
 
   eleventyConfig.addPassthroughCopy('src/static')
 
