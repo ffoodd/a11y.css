dfb1fd55b445af64500239bf57a8954b01b4bd39
ffoodd <gael.poupard@gmail.com>
2021-01-12 21:48:10 +0100

    chore(lint): async completion

--- tasks/css-lint.js
+++ tasks/css-lint.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var path = require('path');
-var gutil = require('gulp-util');
 var csslint = require('gulp-csslint');
 var plumber = require('gulp-plumber');
 var options = require('./helpers/options');
@@ -9,7 +8,7 @@ var onError = require('./helpers/onError');
 csslint.addFormatter('csslint-stylish');
 
 module.exports = function () {
-  gulp.src(options.destination + '/a11y-fr.css')
+  return gulp.src(options.destination + '/a11y-fr.css')
     .pipe(plumber({ errorHandler: onError }))
     .pipe(csslint(path.resolve(__dirname, '..', 'configs', 'csslintrc.json')))
     .pipe(csslint.formatter('stylish'));
