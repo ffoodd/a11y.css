29d09d9792f8a6ce888573c3331e55977d4e966d
Gaël Poupard <gael+github@ffoodd.fr>
2022-02-28 11:43:39 +0100

    docs(syntax highlighting): switch to @ericwbailey's a11y-light Prismjs theme

--- .eleventy.js
+++ .eleventy.js
@@ -6,10 +6,15 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(a11yCSS)
 
   eleventyConfig.addPassthroughCopy('site/static')
-  eleventyConfig.addPassthroughCopy({'css/a11y-en_*-only.css': 'static/css'})
+  eleventyConfig.addPassthroughCopy({
+    'css/a11y-en_errors-only.css': 'static/css/a11y-en_errors-only.css',
+    'css/a11y-en_warnings-only.css': 'static/css/a11y-en_warnings-only.css',
+    'css/a11y-en_obsoletes-only.css': 'static/css/a11y-en_obsoletes-only.css',
+    'css/a11y-en_advices-only.css': 'static/css/a11y-en_advices-only.css',
+    'node_modules/a11y-syntax-highlighting/dist/prism/a11y-light.min.css': 'static/css/a11y-light.min.css'
+  })
   eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`)
   eleventyConfig.addShortcode('version', () => `${Date.now()}`)
-
   eleventyConfig.addWatchTarget('site/static/docs.css')
   eleventyConfig.addWatchTarget('site/static/docs.js')
 
