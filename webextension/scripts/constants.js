3586c189b75924d67edfe9b1f9ced0ef7d15253d
Gaël Poupard <gael+github@ffoodd.fr>
2022-09-12 22:02:06 +0200

    chore(webext): remove webextension folder and assets

--- webextension/scripts/constants.js
+++
@@ -1,9 +0,0 @@
-// CONSTANTS
-
-// browser compatibility
-const BROWSER = chrome || browser;
-const BROWSER_STRING = chrome ? 'chrome' : 'browser';
-// const STORAGE = BROWSER.storage.sync || BROWSER.storage.local;
-
-// a11y.css web extension-specific constants
-const EXTENSION_PREFIX = 'a11ycss_';
