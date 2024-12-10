59eb061b535169fa6c865953408c374223a503f3
Gaël Poupard <gael+github@ffoodd.fr>
2024-12-10 15:07:15 +0100

    chore(site): level-specific CSS files moved to site/ to close #450

--- .eleventy.js
+++ .eleventy.js
@@ -7,15 +7,11 @@ export default function (eleventyConfig) {
 
 	eleventyConfig.addPassthroughCopy('site/static')
 	eleventyConfig.addPassthroughCopy({
-		'css/a11y-en_errors-only.css': 'static/css/a11y-en_errors-only.css',
-		'css/a11y-en_warnings-only.css': 'static/css/a11y-en_warnings-only.css',
-		'css/a11y-en_obsoletes-only.css': 'static/css/a11y-en_obsoletes-only.css',
-		'css/a11y-en_advices-only.css': 'static/css/a11y-en_advices-only.css',
 		'node_modules/a11y-syntax-highlighting/dist/prism/a11y-light.min.css': 'static/css/a11y-light.min.css'
 	})
 	eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`)
 	eleventyConfig.addShortcode('version', () => `${Date.now()}`)
-	eleventyConfig.addWatchTarget('site/static/docs.css')
+	eleventyConfig.addWatchTarget('site/static/css/docs.css')
 	eleventyConfig.addWatchTarget('site/static/docs.js')
 
 	eleventyConfig.setBrowserSyncConfig({
